37fadc6dadb7ac9cb8e913b961e5a311a66d5dc9
littlecapa <littlecapa@googlemail.com>
2025-01-22 19:50:10 +0100

    Fixed Allowed Hosts; now dj-dfm

--- app/dfmproject/settings.py
+++ app/dfmproject/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'dj_dfm',       # Add the service name
+    'dj-dfm',       # Add the service name
 ]
 
 # Application definition
